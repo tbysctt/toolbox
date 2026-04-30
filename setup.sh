e8052d222187d10928fe084efea8a39d5c68460d
Toby Scott <toby@tobyscott.dev>
2026-04-30 17:44:14 +1000

    Add headless sync of Neovim plugins and install fastfetch

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ apk add --no-cache \
   zsh tmux less ripgrep fd fzf grep \
   jq yq unzip gcc musl-dev python3 \
   iputils bind-tools net-tools procps \
-  strace tcpdump traceroute luarocks stow tzdata zsh-vcs neovim shadow
+  strace tcpdump traceroute luarocks stow tzdata zsh-vcs neovim shadow fastfetch
 
 # Git identity defaults
 git config --global user.name "Debug User"
@@ -30,7 +30,6 @@ git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.zsh/z
 # mv nvim-linux-x86_64 /opt/nvim
 # ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
 # rm nvim-linux-x86_64.tar.gz
-# nvim --headless "+Lazy! sync" +qa || true
 
 # Install kubectl
 KUBECTL_VERSION=$(curl -Ls https://dl.k8s.io/release/stable.txt)
@@ -46,6 +45,9 @@ curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/downl
 tar xf lazygit.tar.gz lazygit
 install -Dm755 lazygit /usr/local/bin/lazygit
 rm lazygit.tar.gz lazygit
+
+# Headlessly sync all lazy.nvim plugins
+nvim --headless "+Lazy! sync" +qa || true
 
 # Use ZSH as the default shell
 chsh -s /bin/zsh root
